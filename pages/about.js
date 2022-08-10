f2c07a11bfdfd9cb2308133d20945ed5be26e114
jkim8 <juno0218@naver.com>
2022-08-10 14:39:06 -0400

    routing

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import Navbar from "../components/Navbar";
 
-export default function Home() {
+export default function About() {
   return (
     <div>
       <Navbar />
-      <h1>Hello</h1>
+      <h1>About</h1>
     </div>
   );
 }
